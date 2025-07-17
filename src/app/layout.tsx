ab4a95394d1b3a1b21bce1884d99c38173ea1f95
Benjamin <Benjamin0000rodriguez@gmail.com>
2025-07-16 19:37:50 -0700

    i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Sora } from "next/font/google";
+import { Work_Sans } from "next/font/google"
+import { Special_Gothic_Expanded_One } from "next/font/google";
 import { TRPCReactProvider } from "~/trpc/react";
+import { Manrope } from "next/font/google"
 
 export const metadata: Metadata = {
   title: "Laminor",
@@ -10,18 +13,34 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
-const geist = Geist({
+const sora = Sora({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
-  display: "swap",
+  variable: "--font-sora",
 });
+
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+})
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+})
+
+const specialGothicExpanded = Special_Gothic_Expanded_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-special-gothic-expanded",
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable} font-sans antialiased dark`}>
-      <body className="bg-background text-foreground">
+<html lang="en" className={`${manrope.variable} font-sans antialiased dark`}>
+  <body className="bg-background text-foreground">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-special-gothic-expanded)', 'system-ui', 'sans-serif'],
+        manrope: ['var(--font-manrope)', 'sans-serif'],
+        sora: ['var(--font-sora)', 'sans-serif'],
+        worksans: ['var(--font-work-sans)', 'sans-serif'],
+      },
+    },
+  },
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [],
+  darkMode: "class",
+};
+
+export default config;
